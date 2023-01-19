e00bbd722fa94664e1eb08ab9196ef0660750ffd
egorw <egorw@DESKTOP-TCDMSCK>
2023-01-19 19:56:54 +0300

    feat: Добавлен базовый интерфейс ветви дерева
    Реализован интерфейс ITreeNode по аналогии с IEnumerable.

---
+++ src/Tree/ITreeNode.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+
+namespace OkoloIt.Collections
+{
+    /// <summary>
+    /// Интерфейс ветви дерева, содержащий базовые свойства и метод получения 
+    /// данных.
+    /// </summary>
+    public interface ITreeNode : IEnumerable
+    {
+        /// <summary>
+        /// Уровень вложенности.
+        /// </summary>
+        public int Level { get; }
+
+        /// <summary>
+        /// Свойсво, определяющее является текущая ветвь корнем.
+        /// </summary>
+        public bool IsRoot { get; }
+
+        /// <summary>
+        /// Свойсво, определяющее содержатся ли дочерние элементы.
+        /// </summary>
+        public bool IsLeaf { get; }
+
+        /// <summary>
+        /// Возвращает данные, хранящиеся в ветви дерева.
+        /// </summary>
+        /// <returns>Данные.</returns>
+        public object GetData();
+    }
+}
